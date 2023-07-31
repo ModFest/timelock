3ee2f5d6f68ceabd301a30dc1d977577a38c6ca7
Skye Prince <spiralixp@gmail.com>
2023-07-31 16:56:00 -0700

    actually remove the old code

--- src/main/java/com/acikek/timelock/client/TimelockClient.java
+++ src/main/java/com/acikek/timelock/client/TimelockClient.java
@@ -36,60 +36,8 @@ public class TimelockClient implements ClientModInitializer {
         timelock = new TimelockData(pos, time);
     }
 
-    /*public static long targetTime;
-    public static long increment;
-    public static long targetEnding;
-
-    public static void updateLockingChunk(ChunkPos chunkPos) {
-        lockingChunk = chunkPos;
-        targetTime = timelockData.get(chunkPos);
-        increment = (targetTime - time) / 20L;
-        if (time == -1) {
-            time = getTimeOfDay();
-        }
-    }
-
-    public static void resetLockingChunk(boolean isEnding) {
-        lockingChunk = null;
-        targetTime = -1L;
-        if (isEnding) {
-            targetEnding = estimateEnding();
-            increment = (targetEnding - time) / 20L;
-        }
-    }
-
-    public static boolean canIncrement(long target) {
-        return (increment > 0 && time < target) || (increment < 0 && time > target);
-    }*/
-
-    public static long getTimeOfDay() {
-        return MinecraftClient.getInstance().world.getLevelProperties().getTimeOfDay();
-    }
-
-    /*public static long estimateEnding() {
-        return getTimeOfDay() + 2L;
-    }*/
-
     @Override
     public void onInitializeClient() {
         chunkData.put(new ChunkPos(1, 1), 19000L);
-
-        /*ClientTickEvents.END_WORLD_TICK.register(world -> {
-            if (targetTime != -1L && canIncrement(targetTime)) {
-                System.out.println(increment);
-                time += increment;
-            }
-            else if (targetEnding != -1L && time != -1L) {
-                if (canIncrement(getTimeOfDay())) {
-                    System.out.println(increment);
-                    time += increment;
-                }
-                else {
-                    time = -1L;
-                    increment = 0;
-                    targetEnding = -1L;
-                }
-            }
-        });*/
     }
 }
--- src/main/java/com/acikek/timelock/mixin/LunarWorldViewMixin.java
+++ src/main/java/com/acikek/timelock/mixin/LunarWorldViewMixin.java
@@ -11,12 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LunarWorldView.class)
 public interface LunarWorldViewMixin {
 
-    /*private static float timelock$getSkyAngle(long time) {
-        double d = MathHelper.fractionalPart(time / 24000.0 - 0.25);
-        double e = 0.5 - Math.cos(d * Math.PI) / 2.0;
-        return (float) (d * 2.0 + e) / 3.0f;
-    }*/
-
     @Inject(method = "getSkyAngle", cancellable = true, at = @At(value = "HEAD"))
     private void timelock$modifyTimeOfDay(float tickDelta, CallbackInfoReturnable<Float> cir) {
         TimelockClient.timelock().ifPresent(data -> cir.setReturnValue((float) data.time()));
